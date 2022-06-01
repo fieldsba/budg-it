7a25bf84994eaa0a883e66f2be78067361dff971
Alex Fields <brooke.fields415@gmail.com>
2022-06-01 16:26:51 -0400

    added idb.js for offline functionality and added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,53 @@
+const APP_PREFIX = 'Budget:';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  "index.html",
+  "css/styles.css",
+  "js/idb.js",
+  "js/index.js"
+];
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        return request;
+      } else {       // if no cache, try fetch
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
